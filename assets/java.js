f6759c8caf2d6ffdf178cdb2139591fc466ab462
Nick <nlweber3@gmail.com>
2018-01-19 18:18:08 -0800

    Add files via upload

--- assets/java.js
+++ assets/java.js
@@ -8,3 +8,33 @@
     messagingSenderId: "352822651356"
   };
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var connectionsRef = database.ref("/connections");
+
+  var connectedRef = database.ref(".info/connected");
+
+  connectedRef.on("value", function(snap) {
+  	if (snap.val()) {
+  		var con = connectionsRef.push(true);
+  		con.onDisconnect().remove();
+  	}
+  });
+
+  database.ref().set({
+  	trainName: trainName,
+  	destination: destination,
+  	trainTime: trainTime,
+  	frequency: frequency
+  });
+
+  var trainName = $("#train_name").val().trim();
+  var destination = $("#destination").val().trim();
+  var trainTime = $("#train_time").val().trim();
+  var frequency = $("#frequency").val().trim();
+
+  database.ref("/trainData").on("value", function(snapshot) {
+  	("#train_name").text(snapshot.val().("#name_output");
+  		console.log(snapshot);
+  }
