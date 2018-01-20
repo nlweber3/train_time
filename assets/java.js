e7ae680101fe9bf3a266cf0bb2429f3834dedb88
Nick <nlweber3@gmail.com>
2018-01-19 16:11:22 -0800

    Add files via upload

---
+++ assets/java.js
@@ -0,0 +1,10 @@
+
+  var config = {
+    apiKey: "AIzaSyCoH2QLcmXa5S4KFkTzZdNNM8o3VRze2m8",
+    authDomain: "hw-7-812ac.firebaseapp.com",
+    databaseURL: "https://hw-7-812ac.firebaseio.com",
+    projectId: "hw-7-812ac",
+    storageBucket: "",
+    messagingSenderId: "352822651356"
+  };
+  firebase.initializeApp(config);
